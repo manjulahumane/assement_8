fad1effd9e94faa79b099b251cc0c6f129eafd45
manjulahumane <manjulab.xworkz@gmail.com>
2021-12-20 19:15:43 +0530

    upload

---
+++ Nov.java
@@ -0,0 +1,13 @@
+class Nov{
+static String name="manju";// string(non primitive data type) class can be used as variable
+//static type array
+static String []names={"manju","radhika","lakshmi"}; // string array string is used for store set of character 
+// string is a non primitive data type
+//all non primitive data types are class
+public static void main(String[] args){
+System.out.println(name);
+System.out.println(names[1]);
+
+}
+
+}
